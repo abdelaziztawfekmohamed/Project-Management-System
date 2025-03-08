283c016117c38b7fb25f75cdb04d29ae4a2884f1
abdelaziztawfekmohamed <abdelaziztawfekmohamed.com>
2025-03-08 17:09:36 +0200

    remove unnecessary whitespace in TasksTable component

--- resources/js/Components/TasksTable.tsx
+++ resources/js/Components/TasksTable.tsx
@@ -41,7 +41,6 @@ const TasksTable = ({
   return (
     <>
       <Head title="Tasks" />
-
       {/* {success && (
         <div className="bg-emerald-500 py-2 px-4 text-white rounded mb-4">
           {success}
