6944755e359e43895469444e95914f1d289239ba
abdelaziztawfekmohamed <abdelaziztawfekmohamed.com>
2025-03-16 22:35:39 +0200

    add Post interface, service, and repository for post management functionality

---
+++ app/Interfaces/PostInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces;
+
+use App\Models\Post;
+
+interface PostInterface
+{
+    public function getAllPosts();
+    public function getPaginatedResults($currentUserId);
+    public function getUpvoteCount(Post $post);
+    public function hasUserUpvoted(Post $post);
+    public function hasUserDownvoted(Post $post);
+}
