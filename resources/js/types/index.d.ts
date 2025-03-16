7c873fd7644595998abcb055f374bf1951700fe5
abdelaziztawfekmohamed <abdelaziztawfekmohamed.com>
2025-03-16 21:44:13 +0200

    add TypeScript types for User, Comment, and Post, including permissions and roles

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,12 +1,56 @@
 import { QueryParams } from "./queryParams";
 import { Projects } from "./projects";
+
+// interface Permission {
+//   id: number;
+//   name: string;
+//   created_at?: string;
+//   updated_at?: string;
+// }
+
+// interface Role {
+//   id: number;
+//   name: string;
+//   guard_name: string;
+//   created_at?: string;
+//   updated_at?: string;
+//   permissions?: string[];
+// }
+
 export interface User {
   id: number;
   name: string;
   email: string;
   email_verified_at?: string;
   created_at: string;
+  permissions: string[];
+  roles: string[];
 }
+
+export type PaginatedData<T = any> = {
+  data: T[];
+  links: Record<string, string>;
+};
+
+export type Comment = {
+  id: number;
+  comment: string;
+  created_at: string;
+  user: User;
+};
+
+export type Post = {
+  id: number;
+  name: string;
+  description: string;
+  user: User;
+  created_at: string;
+  upvote_count: number;
+  user_has_upvoted: boolean;
+  user_has_downvoted: boolean;
+  comments: Comment[];
+};
+
 export interface Meta {
   links: {
     url: string | null;
