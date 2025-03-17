bd2be5933bc67a396e1eb28fedca4d70ae3f1440
abdelaziztawfekmohamed <abdelaziztawfekmohamed.com>
2025-03-17 07:55:49 +0200

    Adds comment and upvote functionality
    Implements methods for storing and deleting comments and upvotes.
    Enhances user interaction with posts by allowing authenticated users to manage their comments and upvotes effectively.
    
    Relates to user engagement improvements

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, Post $post)
+    {
+        $data = $request->validate([
+            'comment' => 'required'
+        ]);
+
+        $data['post_id'] = $post->id;
+        $data['created_by'] = Auth::id();
+        Comment::create($data);
+
+        return to_route('post.show', $post);
+    }
+
+    public function destroy(Comment $comment)
+    {
+        if ($comment->user_id !== Auth::id()) {
+            abort(403);
+        }
+        $postId = $comment->post_id;
+        $comment->delete();
+
+        return to_route('post.show', $postId);
+    }
 }
+
+
