4471c4afe065d8ef1eb44b84f7d4e724c89fe4bf
abdulaziztawfekmohamed <abdulaziztawfekmohamed@gmail.com>
2025-03-22 12:59:12 +0200

    Add pagination properties to Projects and Meta interfaces; include current_page and path

--- resources/js/types/projects.ts
+++ resources/js/types/projects.ts
@@ -2,6 +2,7 @@ import { Project } from "./project";
 import { Meta } from "./index";
 
 export interface Projects {
+  current_page: number;
   data: Project[];
   meta: Meta;
 }
--- resources/js/types/tasks.ts
+++ resources/js/types/tasks.ts
@@ -1,6 +1,8 @@
 import { Task } from "./task";
 
 export interface Meta {
+  path: string;
+  current_page: number;
   links: {
     url: string | null;
     label: string;
