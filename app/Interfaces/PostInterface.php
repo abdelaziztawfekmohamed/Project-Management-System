68e29b45751a5c89e3ddc9e1e530aff84d31112d
abdulaziztawfekmohamed <abdulaziztawfekmohamed@gmail.com>
2025-03-22 12:56:14 +0200

    Add getPostComments method to PostInterface for retrieving comments associated with a post

--- app/Interfaces/PostInterface.php
+++ app/Interfaces/PostInterface.php
@@ -7,6 +7,7 @@ use App\Models\Post;
 interface PostInterface
 {
     public function getAllPosts();
+    public function getPostComments(Post $post);
     public function getPaginatedResults($currentUserId);
     public function getUpvoteCount(Post $post);
     public function hasUserUpvoted(Post $post);
